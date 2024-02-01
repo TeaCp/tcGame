9032f570e26b66d612569f0955e55e54b437fbc2
Dn Krsk <wileps2310@yandex.ru>
2024-02-01 18:21:53 +0300

    create plane and char

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,52 @@
+using Godot;
+using System;
+using System.Diagnostics;
+
+public partial class MovementScript : Godot.CharacterBody3D
+{
+	public const float Speed = 5.0f;
+	public const float JumpVelocity = 4.5f;
+
+	// Get the gravity from the project settings to be synced with RigidBody nodes.
+	public float gravity = ProjectSettings.GetSetting("physics/3d/default_gravity").AsSingle();
+
+	private Node3D cleric;
+
+	public override void _Ready()
+	{
+		cleric = GetNode<Node3D>("Cleric2");
+	}
+	
+	private float _t = 0.0f;
+	public override void _PhysicsProcess(double delta)
+	{
+		Vector3 velocity = Velocity;
+		_t += (float)delta * 1.0f;
+
+		// Add the gravity.
+		if (!IsOnFloor())
+			velocity.Y -= gravity * (float)delta;
+
+		// Handle Jump.
+		if (Input.IsActionJustPressed("ui_accept") && IsOnFloor())
+			velocity.Y = JumpVelocity;
+
+		// Get the input direction and handle the movement/deceleration.
+		// As good practice, you should replace UI actions with custom gameplay actions.
+		Vector2 inputDir = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		Vector3 direction = (Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
+		if (direction != Vector3.Zero)
+		{
+			velocity.X = direction.X * Speed;
+			velocity.Z = direction.Z * Speed;
+		}
+		else
+		{
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, Speed);
+			velocity.Z = Mathf.MoveToward(Velocity.Z, 0, Speed);
+		}
+		
+		Velocity = velocity;
+		MoveAndSlide();
+	}
+}
