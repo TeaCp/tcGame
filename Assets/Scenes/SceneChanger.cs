726a8936a03a5e0f6a32e19176f665bd2f560f48
Dn Krsk <wileps2310@yandex.ru>
2024-02-05 16:44:18 +0300

    add new homeScene, add scene change on key

---
+++ Assets/Scenes/SceneChanger.cs
@@ -0,0 +1,38 @@
+using Godot;
+using System;
+
+public partial class SceneChanger : Node3D
+{
+
+	[Export] public NodePath SubViewPortPath;
+
+	private SubViewport _subViewPort;
+	// temporal
+	private PackedScene[] _scenes;
+	private int _sceneIdx = 0;
+	
+	public override void _Ready()
+	{
+		_subViewPort = GetNode<SubViewport>(SubViewPortPath);
+		_scenes = new[]
+		{
+			GD.Load<PackedScene>("res://Assets/Scenes/home_scene.tscn"),
+			GD.Load<PackedScene>("res://Assets/Scenes/demo_scene.tscn")
+		};
+	}
+
+
+	public override void _UnhandledInput(InputEvent @event)
+	{
+		if (@event is InputEventKey eventKey)
+		{
+			if (eventKey.Pressed && eventKey.Keycode == Key.O)
+			{
+				_sceneIdx = Mathf.Wrap(_sceneIdx + 1, 0, _scenes.Length);
+				_subViewPort.GetChild<Node3D>(0).QueueFree();
+				var instance = _scenes[_sceneIdx].Instantiate();
+				_subViewPort.AddChild(instance);
+			}
+		}
+	}
+}
